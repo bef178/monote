6da870041c514ff19eb63f1abf85f525e9872595
tanghao <tanghao.hb@gmail.com>
2019-06-06 23:03:55 +0800

    add generalized SocketAcceptor and Server

---
+++ diarystorage/src/pd/network/Server.java
@@ -0,0 +1,75 @@
+package pd.network;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server extends SocketAcceptor {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
+    private ExecutorService executor;
+
+    private final int numThreads;
+
+    public Server(int port) {
+        this(port, 0);
+    }
+
+    public Server(int port, int numThreads) {
+        super(port);
+        if (numThreads <= 0) {
+            numThreads = Runtime.getRuntime().availableProcessors() + 1;
+        }
+        this.numThreads = numThreads;
+    }
+
+    @Override
+    protected final void onSocket(Socket socket) {
+
+        executor.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                onSocketInWorkerThread(socket);
+                try {
+                    socket.close();
+                } catch (Exception e) {
+                    LOGGER.error("exception when close socket: {}", e.getMessage());
+                }
+            }
+        });
+    }
+
+    /**
+     * override me<br/>
+     */
+    protected void onSocketInWorkerThread(Socket socket) {
+        LOGGER.info("socket handled in worker thread");
+    }
+
+    @Override
+    public void start(Object notifier) throws IOException {
+        if (executor != null && !executor.isShutdown()) {
+            LOGGER.error("executor already running");
+            return;
+        }
+
+        executor = Executors.newFixedThreadPool(numThreads);
+        LOGGER.info("executor created with {} threads", numThreads);
+
+        try {
+            super.start(notifier);
+        } finally {
+            if (executor != null) {
+                executor.shutdown();
+                executor = null;
+                LOGGER.info("executor shutdown");
+            }
+        }
+    }
+}
